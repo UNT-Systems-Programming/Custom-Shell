0ad71e83be826eb38cc49f7c28a0f499cc3a475d
Clayton Brant <Claybrant@live.com>
2016-11-10 12:24:23 -0600

    Update shell.c

--- shell.c
+++ shell.c
@@ -9,15 +9,39 @@ Notes:
 #include <string.h> //strcat
 #include <unistd.h>
 
-#pragma GCC diagnostic ignored "-Wimplicit-function-declaration"
+int launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+  int READ = 0; 					
+  int WRITE = 1;
+  int fileDesc[2];
+  
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
 int main(int argc,char* argv[]){
 	if(argc == 1){
 		// Interactive mode
-		int i,j,status;
-		int READ = 0; 					
-		int WRITE = 1;
-		int fileDesc[2];
-		
+		int i,j,status;		
 		char input[128];
 		char *string[256];
 		char delimit[]=";\n\t\r\v\f";
@@ -42,55 +66,10 @@ int main(int argc,char* argv[]){
 				
 				for(j=0;j<i;j++)
 				{
-					printf("String [%d]=%s\n",j,string[j]);
-					buffer[j] = string[j];				
+					printf("String [%d] = %s\n",j,string[j]);
+					launch(&string[j]);
 				}
-			}
-				if (pipe(fileDesc) == -1)
-				{ 
-					perror("Pipe failed");
-				}
-				
-				int x = 0;
-				switch (fork())
-				{
-					case -1: // Error Checking
-							perror("Error creating fork for command");
-							exit(1);
-			
-					case 0: // First child redirects stdout
-							dup2(fileDesc[WRITE], fileno(stdout));  
-							close(fileDesc[READ]);
-							while(string[x+1] != ' ')
-							{
-								execvp(string[x], &string[x]);
-								x++;
-							}
-										
-					default: // Parent waits for process to terminate
-							wait();			
-							dup2(fileDesc[READ], fileno(stdin));
-							close(fileDesc[WRITE]);
-				}// switch	
-				
-				
-
-				
-				switch( fork() )
-				{
-					case -1:// Error Checking
-							perror("Error creating fork for final command"); 
-					
-					case 0: // Second child redirects stdin
-							dup2(fileDesc[WRITE], fileno(stdout)); 
-							close(fileDesc[READ]);
-							execvp(string[x], &string[x]);
-
-					default: // Parent waits for process to terminate
-							wait();
-							dup2(fileDesc[READ], fileno(stdin));
-							close(fileDesc[WRITE]);
-				}
+			}			
 		}
 		printf("\n");
 		return 0;
