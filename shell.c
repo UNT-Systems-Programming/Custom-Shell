861c449f9cf9194c9288433acf093908174cd2f3
BeardedCoder1337 <williamwolfard@my.unt.edu>
2016-11-14 15:36:02 -0600

    Update shell.c
    Added the extra credit prompt, and tried to add the history but it doesn't work.

--- shell.c
+++ shell.c
@@ -43,16 +43,27 @@ int main(int argc,char* argv[]){
 		// Interactive mode
 		int i,j;		
 		char input[128];
+		char history_buff[128];
 		char *string[256];
+		char prompt[128] = "prompt> ";
+		char *newDir;
+		char *newPrompt;
 		char delimit[]=";\n\t\r\v\f";
 		int exit_flag = FALSE;
+		printf("**** interactive mode ****\n");
+		printf("--> type \"prompt\" at any time to change prompt\n");
+		FILE *history;
+		history = fopen(".history", "w+");
+		if (history == NULL) 
+		{
+			perror("Error opening file");
+			return -1;
+		}
 		while(!exit_flag)
 		{							// Interactive Mode loop
 			i=0;					// Iterator for agrument access
 			j=0;
-			
-			printf("prompt> ");
-			
+			printf("%s",prompt);
 			if(fgets(input, sizeof(input),stdin))
 			{
 				string[i]=strtok(input,delimit);			// Tokenizes first element puts in string
@@ -63,16 +74,35 @@ int main(int argc,char* argv[]){
 				}
 				for(j=0;j<i;j++)
 				{
+					fputs(string[j], history); 
 					if((strstr(string[j],"quit")) != NULL || (strstr(string[j],"exit")) != NULL){
 						exit_flag = TRUE;
 					}
+					else if(strstr(string[j],"cd") != NULL){
+						newDir = strtok(input," ");		// newDir = "cd"
+						newDir = strtok(NULL,"");		// newDir = "Path"
+						chdir(newDir);
+					}
+					else if((strstr(string[j],"prompt")) != NULL){
+						newPrompt = strtok(input," ");	// newPrompt = "prompt"
+						newPrompt = strtok(NULL,"");    // newPrompt = "desired string"
+						strcpy(prompt,newPrompt);		// Copy over
+						strcat(prompt," ");				// Add space
+					}
+					else if((strstr(string[j],"history")) != NULL){
+						printf("Were in history!\n");
+						while(fgets(history_buff,128,(FILE*)history) != '\0'){	// Doesnt work right now
+							printf("History: %s\n",history_buff);
+						}
+					}
 					else{
-						printf("String [%d] = %s\n",j,string[j]);
+						//printf("String [%d] = %s\n",j,string[j]);
 						launch(&string[j]);
 					}
 				}
 				if(exit_flag){
 					printf("\nGoodbye\n");
+					fclose(history);
 					exit(0);
 				}
 			}			
@@ -86,6 +116,7 @@ int main(int argc,char* argv[]){
 		int exit_flag = FALSE;
 
 		char input[128];
+		char *newDir;
 		const char delimit[]=";\n\t\r\v\f";
 		char *string[256];
 
@@ -115,7 +146,12 @@ int main(int argc,char* argv[]){
 				if((strstr(string[j],"quit")) != NULL || (strstr(string[j],"exit")) != NULL){
 						exit_flag = TRUE;
 				}
-				else {
+				else if(strstr(string[j],"cd") != NULL){
+					newDir = strtok(input," ");		// newDir = "cd"
+					newDir = strtok(NULL,"");		// newDir = "Path"
+					chdir(newDir);
+				}
+				else{
 					launch(&string[j]);
 				}
 			}
