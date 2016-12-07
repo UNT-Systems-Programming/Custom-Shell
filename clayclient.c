2f2b467fc4c0e84f325abb888be170fd35a029e9
coding29 <hansajsp29@gmail.com>
2016-12-07 17:28:46 -0600

    Create clayclient.c

---
+++ clayclient.c
@@ -0,0 +1,80 @@
+// Clayton Brant
+// CSCE 3600 Fall 2016
+// 11/21/2016
+// minor 7 client
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
+
+int ticketList [26] = {0};
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[200];
+	char message[200];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+	
+	while(1){
+	    sleep(2);
+		//Receive a reply from the server
+        if( recv(sockfd , buffer , 200 , 0) < 0)
+        {
+            puts("server message retrieval failed");
+			close(sockfd);
+            exit(1);
+        }
+
+	
+	printf("%s%s",buffer,"\n");
+	bzero(buffer,200);
+    bzero(message,200);	
+	fgets(message,200,stdin);
+    n = write(sockfd,message,strlen(message));
+    if (n < 0) 
+         error("ERROR writing to socket");
+    bzero(message,200);
+    
+    if (n < 0) 
+         error("ERROR reading from socket");
+    printf("%s\n",message);
+    
+	}
+	
+	
+	close(sockfd);
+    return 0;
+}
